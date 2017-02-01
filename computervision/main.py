5426dba1f61c187fb9a2f8c12ad243918e9e838e
hoppsann3 <einararbo@gmail.com>
2017-02-01 14:29:11 +0100

    Snapshot with sounds, annoying as f

--- computervision/main.py
+++ computervision/main.py
@@ -1,7 +1,56 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
+import sys
 
-if __name__ == '__main__':
-    print 'hello world'
+
+def histogramEqualization(image, plot=False):
+    equ = cv2.equalizeHist(image)
+    if plot:
+        plt.hist(image.ravel(),256,[0,256]); plt.show()
+        plt.hist(equ.ravel(),256,[0,256]); plt.show()
+    return equ
+
+def show_picture(image, title=''):
+    cv2.imshow(title, image)
+    cv2.waitKey(0)
+
+def canny(img):
+    edges = cv2.Canny(img, 0, 150, L2gradient=True)
+    show_picture(edges)
     
+def sharpen(img, k):
+    if k == 1:
+        kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+    elif k == 2:
+        kernel = np.array([[1,1,1], [1,-7,1], [1,1,1]])
+    elif k == 3:
+        kernel = np.array([[-1,-1,-1,-1,-1],
+                                 [-1,2,2,2,-1],
+                                 [-1,2,8,2,-1],
+                                 [-1,2,2,2,-1],
+                                 [-1,-1,-1,-1,-1]]) / 8.0
+
+
+    output = cv2.filter2D(img, -1, kernel)
+    show_picture(output, 'kernel' + str(k))
+    return output
+    
+if __name__ == '__main__':
+    kernelSize = 7
+    kernelSize = (kernelSize, kernelSize)
+    filename = sys.argv[1]
+
+    image = cv2.imread(filename, 0)
+    image = cv2.GaussianBlur(image, kernelSize, 0)
+    image = sharpen(image, 1)
+    image = cv2.medianBlur(image, 7)
+    
+    image = cv2.GaussianBlur(image, kernelSize, 0)
+    image = cv2.adaptiveThreshold(image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+                                                   cv2.THRESH_BINARY,11,2)
+
+    image = cv2.GaussianBlur(image, kernelSize, 0)
+    image = cv2.Canny(image, 0, 150, L2gradient=True)
+    cv2.imshow('Edgedetection', image)
+    cv2.waitKey(0)
--- computervision/video.py
+++ computervision/video.py
@@ -1,30 +1,59 @@
 import cv2
 import numpy as np
-
+import pygame
 
 #Sets up videostream from video source 0.
 #Press s to take snapshot
 #Press q to quit 
 
+def play_sound(filename):
+    pygame.init()
+    pygame.mixer.music.load(filename)
+    pygame.mixer.music.play()
+
+
 def save_image(frame):
+
     random_integer = np.random.randint(100000)
     filename = 'images/snapshot' + str(random_integer) + '.png'
-    print 'Writing file: {}'.format(filename)
-    cv2.imwrite(filename, frame)
 
-if __name__ == '__main__':
+    try:
+        cv2.imwrite(filename, frame)
+        print 'Writing file: {}'.format(filename)
+    except:
+        print 'Could not write file'
+
+def start_video_stream(grayScale=True, sound=False):
     cap = cv2.VideoCapture(0)
     while True:
         ret, frame = cap.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        if grayScale:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-        cv2.imshow('frame', gray)
+        cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('s'):
-            cv2.imshow('Snapshot', gray)
-            save_image(gray)
+            if sound:
+                play_sound('click.wav')
+            cv2.imshow('Snapshot', frame)
+            save_image(frame)
         elif cv2.waitKey(1) & 0xFF == ord('q'):
             break
         
     cap.release()
     cv2.destroyAllWindows()
     pass
+
+def take_snapshot(grayScale=True, sound=False):
+    cap= cv2.VideoCapture(0)
+    ret, frame = cap.read()
+    if grayScale:
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('SNAPSHOT', frame)
+
+    play_sound('click.wav')
+    save_image(frame)
+    cv2.waitKey(0)
+    
+if __name__ == '__main__':
+    start_video_stream(False)
+    #take_snapshot()
