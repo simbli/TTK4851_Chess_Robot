8d626ffb6a755b973631b7b2141bab73978bf73e
hoppsann3 <einararbo@gmail.com>
2017-02-01 15:11:47 +0100

    Working on bounding boxes and contours

--- computervision/main.py
+++ computervision/main.py
@@ -3,6 +3,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sys
 
+def findContours(edges):
+    contours = cv2.findContours(edges.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    return contours[1]
+
+def printContours(image, contours):
+    print 'found {} contours total'.format(len(contours))
+    for c in contours:
+#        (x,y,w,h) = cv2.boundingRect(c)
+        cv2.drawContours(image, [c], -1,  (0,255,0),3)
+        cv2.imshow('', image)
+        cv2.waitKey(0)
+
+def getCentroid(contour):
+    M = cv2.moments(contour)
+    cx = int(M['m10']/M['m00'])
+    cy = int(M['m01']/M['m00'])
+    return cx, cy
 
 def histogramEqualization(image, plot=False):
     equ = cv2.equalizeHist(image)
@@ -37,20 +54,32 @@ def sharpen(img, k):
     return output
     
 if __name__ == '__main__':
-    kernelSize = 7
-    kernelSize = (kernelSize, kernelSize)
-    filename = sys.argv[1]
+    k = 7
+    sharpeningKernelType = 1
+    kernelSize = (k, k)
 
-    image = cv2.imread(filename, 0)
-    image = cv2.GaussianBlur(image, kernelSize, 0)
-    image = sharpen(image, 1)
-    image = cv2.medianBlur(image, 7)
     
+    #Open image with argv[1]
+    filename = sys.argv[1]
+    image = cv2.imread(filename, 0)
+    show_picture(image, 'Original image')
+    
+    #Image-preprocessing to make edge detection easier
+    image = cv2.GaussianBlur(image, kernelSize, 0)
+    show_picture(image, 'Blurred with {}x{} gaussian kernel'.format(k,k))
+    image = sharpen(image, sharpeningKernelType)
+    show_picture(image, 'Sharpened image using kernel {}'.format(sharpeningKernelType)) 
+    image = cv2.medianBlur(image, 7)
     image = cv2.GaussianBlur(image, kernelSize, 0)
     image = cv2.adaptiveThreshold(image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
                                                    cv2.THRESH_BINARY,11,2)
-
     image = cv2.GaussianBlur(image, kernelSize, 0)
+
+    #Canny edge detection to find edges
     image = cv2.Canny(image, 0, 150, L2gradient=True)
+
+    #Contour detection to find contours of pieces
+#    contours = findContours(image)
+#    printContours(image, contours)
     cv2.imshow('Edgedetection', image)
     cv2.waitKey(0)
