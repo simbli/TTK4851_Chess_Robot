a6e526c0deb88b2c431f6322e433be123eed61b3
Eirik Bertelsen <xecor2k@hotmail.com>
2017-02-15 14:48:04 +0100

    added stockfish vs stockfish

---
+++ ai/stockfish_vs_stockfish.py
@@ -0,0 +1,28 @@
+from stockfish import Stockfish
+import chess
+import time
+
+board = chess.Board()
+
+stockfish = Stockfish()
+
+moves = []
+
+while True:
+	moves.append(stockfish.get_best_move())
+	stockfish.set_position(moves)
+	board.push(chess.Move.from_uci(moves[len(moves)-1]))
+
+	print "computer 1 made move:",moves[len(moves)-1]
+	print board 
+
+	time.sleep(3)
+
+	moves.append(stockfish.get_best_move())
+	stockfish.set_position(moves)
+	board.push(chess.Move.from_uci(moves[len(moves)-1]))
+
+	print "computer 2 made move:",moves[len(moves)-1]
+	print board 
+
+	time.sleep(3)
