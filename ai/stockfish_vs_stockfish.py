28db5dff4801fe71c7d2795f4c4488832936ab4e
Eirik Bertelsen <xecor2k@hotmail.com>
2017-02-22 10:44:36 +0100

    made changed to the java socket setup

--- ai/stockfish_vs_stockfish.py
+++ ai/stockfish_vs_stockfish.py
@@ -4,7 +4,8 @@ import time
 
 board = chess.Board()
 
-stockfish = Stockfish()
+stockfish = Stockfish(depth=2)
+stockfish2 = Stockfish(depth=20)
 
 moves = []
 
@@ -13,16 +14,17 @@ while True:
 	stockfish.set_position(moves)
 	board.push(chess.Move.from_uci(moves[len(moves)-1]))
 
-	print "computer 1 made move:",moves[len(moves)-1]
+	print "shallow AI made move:",moves[len(moves)-1]
 	print board 
 
 	time.sleep(3)
 
-	moves.append(stockfish.get_best_move())
-	stockfish.set_position(moves)
+	moves.append(stockfish2.get_best_move())
+	stockfish2.set_position(moves)
 	board.push(chess.Move.from_uci(moves[len(moves)-1]))
 
-	print "computer 2 made move:",moves[len(moves)-1]
+	print "deep AI made move:",moves[len(moves)-1]
 	print board 
-
+	print
+	print
 	time.sleep(3)
