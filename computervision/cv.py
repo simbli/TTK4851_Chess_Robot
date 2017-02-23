9388350089a2c1bbfc0b72cd1ecc73a5ea27df8a
hoppsann3 <einararbo@gmail.com>
2017-02-23 17:26:02 +0100

    Added castling (rokade)

--- computervision/cv.py
+++ computervision/cv.py
@@ -42,15 +42,28 @@ class Compvision():
         else:
             diff_index = np.where(self.prev_board != self.board_to_compare)
             if len(diff_index[0]) == 2:
-                color_val_from = self.prev_board[diff_index[0][0]][diff_index[1][0]]
-                color_val_to = self.prev_board[diff_index[0][1]][diff_index[1][1]]
+                color_val_from = self.board_to_compare[diff_index[0][0]][diff_index[1][0]]
+                color_val_to = self.board_to_compare[diff_index[0][1]][diff_index[1][1]]
                 frm =  chr(diff_index[1][0] + 97) +  str(8 - diff_index[0][0])
                 to = chr(diff_index[1][1] + 97) + str(8 - diff_index[0][1])
                 #If we moved the other way, flip the values
-                if color_val_to > 0:
+                if color_val_to == 0:
                     (frm, to) = (to, frm)
                 return frm + to
+            elif len(diff_index[0]) == 4:
+                if all(self.prev_board[7][4:] == [1,0,0,1]):
+                    return 'e1g1'
+                elif all(self.prev_board[7][:5] == [1,0,0,0,1]):
+                    return 'e1c1'
+                elif all(self.prev_board[0][4:] == [2,0,0,2]):
+                    return 'e8g8'
+                elif all(self.prev_board[0][:5] == [2,0,0,0,2]):
+                    return 'e8c8'
+                else:
+                    print 'an error occured'
+
             else:
+                print len(diff_index[0])
                 print 'an error occured'
                 return False
 
@@ -65,28 +78,59 @@ def test(case_number):
     c.prev_board = prev_board
     c.board_to_compare = board
     if case_number == 1:
-        board[1][1] = 0
-        board[2][1] = 2
+        c.prev_board[1][1] = 0
+        c.board_to_compare[2][1] = 2
         assert c.check_if_move_is_correct('b7b6')
         assert c.get_move() == 'b7b6', 'Test 1 failed'
 
     elif case_number == 2:
-        board[6][0] = 0
-        board[5][0] = 1
+        c.board_to_compare[6][0] = 0
+        c.board_to_compare[5][0] = 1
+
         assert c.check_if_move_is_correct('a2a3')
         assert c.get_move() == 'a2a3', 'Test 2 failed'
 
     elif case_number == 3:
-        board[6][0] = 0
-        board[1][0] = 1
+        c.board_to_compare[6][0] = 0
+        c.board_to_compare[1][0] = 1
         assert c.check_if_move_is_correct('a2a7')
         assert c.get_move() == 'a2a7', 'Test 3 failed'
 
     elif case_number == 4:
-        board[1][0] = 0
-        board[6][0] = 2
+        c.board_to_compare[1][0] = 0
+        c.board_to_compare[6][0] = 2
         assert c.check_if_move_is_correct('a7a2')
         assert c.get_move() == 'a7a2', 'Test 4 failed'
+
+    elif case_number == 5:
+        c.prev_board[7] = [1,0,0,0,1,1,1,1]
+        c.board_to_compare[7] = [0,0,1,1,0,1,1,1]
+        print c.prev_board
+        print '\n'
+        print c.board_to_compare
+        assert c.check_if_move_is_correct('e1c1')
+        assert c.get_move() == 'e1c1', 'Test 4 failed'
+
+    elif case_number == 6:
+        c.prev_board[7] = [1,1,1,1,1,0,0,1]
+        c.board_to_compare[7] = [1,1,1,1,0,1,1,0]
+        print c.prev_board
+        print c.board_to_compare
+        assert c.check_if_move_is_correct('e1g1')
+        assert c.get_move() == 'e1g1', 'Test 4 failed'
+
+    elif case_number == 7:
+        c.prev_board[0] = [2,0,0,0,2,2,2,2]
+        c.board_to_compare[0] = [0,0,2,2,0,2,2,2]
+        assert c.check_if_move_is_correct('e8c8')
+        assert c.get_move() == 'e8c8', 'Test 4 failed'
+
+    elif case_number == 8:
+        c.prev_board[0] = [2,2,2,2,2,0,0,2]
+        c.board_to_compare[0] = [2,2,2,2,0,2,2,0]
+        assert c.check_if_move_is_correct('e8g8')
+        assert c.get_move() == 'e8g8', 'Test 4 failed'
+
 if __name__ == '__main__':
 
     #Test cases:
@@ -95,8 +139,13 @@ if __name__ == '__main__':
     #3: white captures black
     #4: Black captures white
     #Special cases:
-    #5: Promotion
-    #6: Castling
-    for i in range(1,4):
+    #6: Promotion
+    #Castling white
+        #5: Castling queens side
+        #6: Castling kings side
+    #7: Castling black
+        #7: Castling queens side
+        #8: Castling kings side
+    for i in range(1,8):
         test(i)
     print 'All tests passed'
