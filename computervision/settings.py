e43e08abbedf7a8f0868e013a2bdc62e1073ba37
hoppsann3 <einararbo@gmail.com>
2017-03-09 14:04:43 +0100

    Added setting file, unsure whether to use or not

---
+++ computervision/settings.py
@@ -0,0 +1,14 @@
+#File using the most common variables for easier tuning
+CHESSBOARD_SIZE = 8
+INTERNAL_CORNERS = 7
+MAT_SIZE = 9
+MASK_RADIUS = 1
+EPSILON = 1e-9
+
+TH_BLACK_WHITE = 70 #Threshold for separating black/white pieces
+CANNY_LOWER = 25 #Lower threshold for canny edge detector
+CANNY_HIGHER = 90 #Higher threshold for canny edge detector
+W_MAX = 47 #Max width of contours to be accepted as pieces
+W_MIN = 10 #Min width of contours to be accepted as pieces
+H_MAX = 47 #Max heigth of contours to be accepted as pieces
+H_MIN = 10 #Min heigth of contours to be accepted as pieces
