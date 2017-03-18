2c98f1d724db83c58562dddbd9e0518b768a7571
hoppsann3 <einararbo@gmail.com>
2017-03-18 17:36:01 +0100

    Cleanup

--- computervision/video.py
+++ computervision/video.py
@@ -1,16 +1,10 @@
 import cv2
 import numpy as np
-import pygame
 import sys
 
 #Sets up videostream from video source 0.
 #Press s to take snapshot
 #Press q to quit
-
-def play_sound(filename):
-    pygame.init()
-    pygame.mixer.music.load(filename)
-    pygame.mixer.music.play()
 
 
 def save_image(frame):
@@ -24,21 +18,11 @@ def save_image(frame):
     except:
         print 'Could not write file'
 
-"""
-def start_video_stream(grayScale=True, sound=False):
-
-    play_sound('click.wav')
-    random_integer = np.random.randint(100000)
-    filename = 'images/snapshot' + str(random_integer) + '.png'
-    print 'Writing file: {}'.format(filename)
-    cv2.imwrite(filename, frame)
-"""
 
 def capture_video(output_filename):
     cap = cv2.VideoCapture(1)
 
     # Define the codec and create VideoWriter object
-    #fourcc = cv2.VideoWriter_fourcc(*'XVID')
     filename = output_filename + '.avi'
     fourcc = cv2.cv.CV_FOURCC(*'XVID')
     out = cv2.VideoWriter(filename,fourcc, 20.0, (640,480))
@@ -46,9 +30,8 @@ def capture_video(output_filename):
     while(cap.isOpened()):
         ret, frame = cap.read()
         if ret==True:
-            #frame = cv2.flip(frame,0)
 
-            # write the flipped frame
+            # write the frame
             out.write(frame)
 
             cv2.imshow('frame',frame)
@@ -62,7 +45,7 @@ def capture_video(output_filename):
     out.release()
     cv2.destroyAllWindows()
 
-def start_video_stream(grayScale=False, sound=False):
+def start_video_stream(grayScale=False):
 
     cap = cv2.VideoCapture(1)
     while True:
@@ -72,9 +55,6 @@ def start_video_stream(grayScale=False, sound=False):
         frame = cv2.flip(frame,-1)
         cv2.imshow('frame', frame)
         if cv2.waitKey(1) & 0xFF == ord('s'):
-
-            if sound:
-                play_sound('click.wav')
 
             cv2.imshow('Snapshot', frame)
             save_image(frame)
